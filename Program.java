2b9504eba470d25eeece22f50beda842104696de
Aditya Dhumal <adityad@heaptrace.com>
2025-03-28 19:22:42 +0530

    Initial commit print 3 lines of code

---
+++ Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is line 1!");
+        System.out.println("Welcome to Java programming, this is line 2!");
+        System.out.println("Have a great day, this is line 3!");
+    }
+}
